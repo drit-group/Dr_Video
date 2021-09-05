8989bcd1c9b1282793bb258fb8e363ac1d404643
darya khani <nargeskhani.1379@gmail.com>
2021-09-05 04:46:59 -0700

    Editedsingle

--- landing/views.py
+++ landing/views.py
@@ -2,13 +2,17 @@ from django.shortcuts import render , get_object_or_404
 from django.http import HttpResponse
 from account.models import Article
 from django.views.generic import ListView , DetailView
-from django.core.paginator import Paginator
+#from django.core.paginator import Paginator
 # Create your views here.
 
 class ListVideo(ListView):
     model = Article
     template_name = "landing/main.html"
     paginate_by = 2
+
+class DetailVideo(DetailView):
+    model = Article
+    template_name = "landing/single.html"
 
 # def home(request,page=None):
 #     name = request.GET.get("name")
@@ -19,26 +23,9 @@ class ListVideo(ListView):
 #     contex = { 'Article' : page_obj}
 #     return render(request,'landing/index.html',contex)
 
-class DetailVideo(DetailView):
-    model = Article
-    template_name = "landing/single.html"
+
 
 # def detail(request,pk_id):
 #     video = get_object_or_404(Vidoes, pk=pk_id )
 #     return render(request, "landing/single.html", {'video':video})
 
-def multi(request,num):
-    num = int(num)
-    num += 1
-    contex = []
-    for i in range(1,num):
-        tmp = []
-        for j in range(1,num):
-            tmp.append(j*i)
-        contex.append(tmp)
-    return render(request, "contex.html", {"contex" : contex})
-# contex = [
-# [1,2,3],
-# [1,2,3],
-# [1,2,3],
-# ]
