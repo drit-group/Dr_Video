e65715e8c8c87c93741f41f334c3e4eb71f44334
darya khani <nargeskhani.1379@gmail.com>
2021-09-06 03:07:31 -0700

    editedfiles

--- landing/views.py
+++ landing/views.py
@@ -9,24 +9,14 @@ class ListVideo(ListView):
     model = Article
     template_name = "landing/main.html"
     paginate_by = 2
-<<<<<<< Updated upstream
-    #use oBject_list as a oBjectlist
-=======
 
-def home(request,page=None):
-    name = request.GET.get("name")
-    objects = Article.objects.all()
-    objects = Paginator(objects,2)
-    page_number = page
-    page_obj = objects.get_page(page_number)
-    contex = { 'Article' : page_obj}
-    return render(request,'landing/main.html',contex)
->>>>>>> Stashed changes
+
+
 
 class DetailVideo(DetailView):
     model = Article
     template_name = "landing/single.html"
-    #use Article as a oBjectlist.Becouse of it's one ithem
+    #use Article or object as an objectlist.Becouse of it's one ithem
 
 # def home(request,page=None):
 #     name = request.GET.get("name")
@@ -42,13 +32,3 @@ class DetailVideo(DetailView):
 # def detail(request,pk_id):
 #     video = get_object_or_404(Vidoes, pk=pk_id )
 #     return render(request, "landing/single.html", {'video':video})
-
-<<<<<<< Updated upstream
-=======
-
-# contex = [
-# [1,2,3],
-# [1,2,3],
-# [1,2,3],
-# ]
->>>>>>> Stashed changes
