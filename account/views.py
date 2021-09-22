866fa08c9fe28574dc170702eea9b82d2237a28f
darya khani <nargeskhani.1379@gmail.com>
2021-09-22 08:52:01 -0700

    addold

--- account/views.py
+++ account/views.py
@@ -1,9 +1,11 @@
+from account.mixins import formMixin
 from django.db import models
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.views.generic import ListView
+from django.views.generic import ListView ,CreateView
 from .models import Article
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
 # Create your views here.
 
 class DashBoard(LoginRequiredMixin,ListView):
@@ -13,9 +15,9 @@ class DashBoard(LoginRequiredMixin,ListView):
     def get_queryset(self):
         return Article.objects.filter(writer=self.request.user)
 
-    # model = Article
-    login_url = "/admin/login"
-    template_name = "profile/main.html"
-    def get_queryset(self):
-        return Article.objects.filter(writer=self.request.user)
+class addArticle(formMixin,LoginRequiredMixin,CreateView):
+    model = Article
+    template_name = "profile/addArticle.html"
+    fields=['title','slug','thumbnail','description','status']
+    success_url=reverse_lazy("account:dashboard")
 
