8bc603efdb1453e927dd251f82d1aa670ff3b37b
darya-nrgs <nargeskhani.1379@gmail.com>
2021-10-15 09:06:11 -0700

    landing:single.html edited

--- account/mixins.py
+++ account/mixins.py
@@ -7,7 +7,7 @@ class formMixin():
         return super().form_valid(form)
 class Status_access():
     def dispatch(self,request,pk=None):
-        if (request.user.is_admin):
+        if (request.user.is_superuser or request.user.is_admin):
             self.fields = ['title','slug','thumbnail','description','status','category','source']
         elif (request.user.is_writer):
             self.fields = ['title','slug','thumbnail','description','category','source']
