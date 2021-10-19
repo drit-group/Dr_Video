5724f0b3e2fcbb054fe48d5f35bbf5fa6749583e
darya-nrgs <nargeskhani.1379@gmail.com>
2021-10-19 05:33:55 -0700

    omit few things

--- account/templatetags/random.py
+++ account/templatetags/random.py
@@ -1,8 +1,8 @@
-from django import template
-from random import randint
+# from django import template
+# from random import randint
 
-register = template.Library()
+# register = template.Library()
 
-@register.simple_tag
-def gen_random():
-    return randint(0,200)
+# @register.simple_tag
+# def gen_random():
+#     return randint(0,200)
